44a3d234d7d4e52e397c10b83593b2b1da17d08e
lapin <serprev.z@gmail.com>
2019-01-22 16:03:20 +0100

    correction de la condition dans la vue SendEventReportView + ajout de tests de la vue et de la tache send_event_report

--- agir/events/tests/test_tasks.py
+++ agir/events/tests/test_tasks.py
@@ -29,6 +29,7 @@ class EventTasksTestCase(TestCase):
             location_zip="75014",
             location_city="Paris",
             location_country="FR",
+            report_content="Ceci est un resumé!\navec un retoru à la ligne",
         )
 
         self.organizer_config = OrganizerConfig.objects.create(
@@ -129,3 +130,11 @@ class EventTasksTestCase(TestCase):
             self.assert_(str(tasks.CHANGE_DESCRIPTION["information"]) in text)
             self.assert_(str(tasks.CHANGE_DESCRIPTION["timing"]) in text)
             self.assert_(str(tasks.CHANGE_DESCRIPTION["contact"]) not in text)
+
+    def test_send_event_report_mail(self):
+        tasks.send_event_report(self.event.pk)
+        self.assertEqual(len(mail.outbox), 2)
+
+        text = mail.outbox[0].body
+        self.assert_(self.event.name in text)
+        self.assert_(self.event.report_content[:100] in text)
